4f91d6c5a17503007eaed3809434512d6a7039cb
Hubertt5 <78238119+Hubertt5@users.noreply.github.com>
2023-02-26 23:04:26 +0100

    Add files via upload

---
+++ js/quizzes/GOB_script.js
@@ -0,0 +1,45 @@
+const quizData = [
+  {
+    question: "Jaki kształt ma Sztandar naszej szkoły?",
+    a: "prostokąt",
+    b: "kwadrat",
+    c: "rąb",
+    d: "trójkąt",
+    correct: "b",
+  },
+  {
+    question: "Ile mierzy bok sztandaru?",
+    a: "80cm",
+    b: "90cm",
+    c: "100cm",
+    d: "110cm",
+    correct: "c",
+  },
+  {
+    question: "Z jakich dwóch głównych kolorów składa się sztandar?",
+    a: "zielonego i czerwonego",
+    b: "złotego i zielonego",
+    c: "żółtego i zielonego",
+    d: "złotego i czerwonego",
+    correct: "a",
+  },
+
+  {
+    question:
+      "Co znajduje się w jego centralnej części?",
+    a: "herb Warszawy",
+    b: "Flaga Polski",
+    c: "logo technikum",
+    d: "Napis - ,,Pamiętamy''",
+    correct: "c",
+  },
+  {
+    question:
+      "Co znajduje się w centrum loga szkoły?",
+    a: "Człowiek",
+    b: "Teleskop",
+    c: "Luneta",
+    d: "Teodolit",
+    correct: "d",
+  },
+];
