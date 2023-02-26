dd82ddb16c922921576b6c21460ba79354465a33
Hubertt5 <78238119+Hubertt5@users.noreply.github.com>
2023-02-26 16:38:39 +0100

    Add files via upload

---
+++ js/quizzes/CLOY_script.js
@@ -0,0 +1,44 @@
+const quizData = [
+  {
+    question: "Jak się nazywa Patron szkoły zs14?",
+    a: "Prof. Stanisław Kliniak",
+    b: "Prof. dr Stanisław Kluźniak",
+    c: "Dr Szymon Kluźniak",
+    d: "Prof. dr Stanisław Kuźniak",
+    correct: "b",
+  },
+  {
+    question:
+      "Kiedy się urodził?",
+    a: "19 września 1892 roku",
+    b: "17 września 1891 roku",
+    c: "16 września 1893 roku",
+    d: "15 września 1890 roku",
+    correct: "b",
+  },
+  {
+    question: "Gdzie się urodził?",
+    a: "W Krakowie",
+    b: "W Warszawie",
+    c: "W Hebdowie Miechowskim",
+    d: "W Poznaniu",
+    correct: "c",
+  },
+
+  {
+    question: "Z jakiej dziedziny słyną ten patron?",
+    a: "Z informatyki",
+    b: "Z grafiki komputerowej",
+    c: "Z Geodezji",
+    d: "Z gastronomij",
+    correct: "c",
+  },
+  {
+    question: "Kiedy zmarł?",
+    a: "1 października 1957 rok",
+    b: "1 września 1958 rok",
+    c: "1 listopada 1955 rok",
+    d: "1 sierpnia 1956 rok",
+    correct: "a",
+  },
+];
